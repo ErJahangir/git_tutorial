a5f7cf9bf8abae3c789970a85bb17bbd1848dfff
jahangir <jahangiralamnke786@gmail.com>
2024-12-23 18:00:02 +0530

    c file deleted

--- new.js
+++ new.js
@@ -1,6 +1,12 @@
-let str = "My name is jahangir alam";
+let str = "my name is jahangir alam";
 
-const str1 = str.split(" ").reverse().join(" ");
+const str1 = str.split(" ");
 
 console.log(str);
 console.log(str1);
+const str2 = [];
+for (let i of str1) {
+  //   console.log(i);
+  str2.push(i.charAt(0).toUpperCase() + i.substring(1));
+}
+console.log(str2.join(" "));
