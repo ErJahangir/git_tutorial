0593b9fca4d23fe4c70cefa760131733acbb48f7
jahangir <jahangiralamnke786@gmail.com>
2024-12-23 16:47:08 +0530

    updated

--- new1.js
+++ new1.js
@@ -1,8 +1,9 @@
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var arr = [1, 2, 30, 14, 5, 6, 7, 8, 9, 10];
 
-for (var k in arr) {
-  console.log(k);
+// for (var i of arr) {
+//   console.log(i);
+// }
+
+for (var i in arr) {
+  console.log({ [i]: arr[i] });
 }
-
-console.log("vikash Kumar gupta");
-
